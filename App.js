f2d4351ee2628c3b2e83f36d955b6f59587618cb
mujaffar09 <110367256+mujaffar09@users.noreply.github.com>
2022-11-03 00:09:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Datafetching from "./Phonebook/Datafetching";
+import Addnames from "./Phonebook/Addnames";
+
+
+function App() {
+  return (
+    <div>
+      
+      {/* <Datafetching /> */}
+      <Addnames />
+      
+    </div>
+  );
+}
+
+export default App;
